207419ea7acfa964a385f7c0179b5502251f368e
gevera <skryonline@gmail.com>
2022-06-13 23:31:39 +0500

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#b29a87',
+
+					secondary: '#baf1ff',
+
+					accent: '#dd8f54',
+
+					neutral: '#2C203C',
+
+					'base-100': '#E6E4E7',
+
+					info: '#50C5DC',
+
+					success: '#4ED4A3',
+
+					warning: '#F5990F',
+
+					error: '#FC2C56'
+				}
+			}
+		]
+	},
+
+	plugins: [require('daisyui')]
+};
+
+module.exports = config;
